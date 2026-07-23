2752a1a122fe0d08b5faf9d3b5d4950293822770
ernestoperezfraga911024 <ernestoperezfraga911024@gmail.com>
2026-07-23 12:08:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cuban-league-v13-premium-mobile-fixed';
+const CACHE='cuban-league-v14-history-tables-mobile';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
