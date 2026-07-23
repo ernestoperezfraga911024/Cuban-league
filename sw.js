ca474adc4490fcf2b4ff31f6d762df638a40b5c4
ernestoperezfraga911024 <ernestoperezfraga911024@gmail.com>
2026-07-23 09:47:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,4 @@
-const CACHE='cuban-league-v6-mobile';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll([
-      './',
-      'index.html',
-      'styles.css?v=6-mobile',
-      'app.js?v=6-mobile',
-      'data.json'
-    ]))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then(keys =>
-        Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
-      )
-    ])
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
+const CACHE='cuban-league-v7-true-mobile';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x))))]));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
