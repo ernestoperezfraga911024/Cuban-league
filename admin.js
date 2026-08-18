580e86049594be15c9959db1af46d0c1acd43f76
ernestoperezfraga911024 <ernestoperezfraga911024@gmail.com>
2026-08-18 16:18:08 -0400

    Añade a Rodri Hernández al catálogo maestro

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '131-20260818';
+  const VERSION = '132-20260818';
   const OWNER_VISIT_EXCLUSION_KEY = 'cuban-league-owner-browser';
   const LOCAL_DRAFT_PREFIX = 'cuban-admin-draft:';
   const ARCHIVED_DRAFT_PREFIX = 'cuban-admin-archived-draft:';
