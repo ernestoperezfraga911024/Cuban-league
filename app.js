b965df6310d8c3159cc971d112f589a51c5446a2
ernestoperezfraga911024 <ernestoperezfraga911024@gmail.com>
2026-08-18 15:02:57 -0400

    Añade el apodo de Esteban Dominguez

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION='130-20260818';
+const APP_VERSION='131-20260818';
 const OWNER_VISIT_EXCLUSION_KEY='cuban-league-owner-browser';
 const ACHIEVEMENT_SEEN_KEY='cuban-league-seen-achievements-v1';
 let DATA;
