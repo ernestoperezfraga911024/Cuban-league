088d48929122cd909a92c5a5372058d7d5ab72b8
ernestoperezfraga911024 <ernestoperezfraga911024@gmail.com>
2026-09-01 12:55:40 -0400

    V150: corrige navegación secundaria móvil

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION='150-20260901';
+const APP_VERSION='150-20260901-r2';
 const OWNER_VISIT_EXCLUSION_KEY='cuban-league-owner-browser';
 const ACHIEVEMENT_SEEN_KEY='cuban-league-seen-achievements-v1';
 let DATA;
